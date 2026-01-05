50b12df42e1ec1b0cbde29832c60144f6d60597c
takeshid <goriponikeike55@gmail.com>
2026-01-05 23:33:45 +0900

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            markdown-oxide
+            astro-language-server
+            vscode-langservers-extracted
+          ];
+        };
+      }
+    );
+}
